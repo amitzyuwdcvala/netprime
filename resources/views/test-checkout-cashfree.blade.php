576e7d7921f77dde55601d1c1a4c92bd3e4f4b45
flixygo <flixygo2026@gmail.com>
2026-03-18 23:30:44 +0530

    update the key and update the sidebar content

--- resources/views/test-checkout-cashfree.blade.php
+++ resources/views/test-checkout-cashfree.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test Cashfree Checkout – NetPrime</title>
+    <title>Test Cashfree Checkout – FlixyGO</title>
     <script src="https://sdk.cashfree.com/js/v3/cashfree.js"></script>
     <style>
         body {
