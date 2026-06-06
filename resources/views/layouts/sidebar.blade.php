bc1bc955def5e01d2d94210053ce39ddc4a291bf
amitzyuwdcvala <amitvala065@gmail.com>
2026-06-07 00:37:13 +0530

    Use APP_NAME in sidebar and fix deploy for zoominfosys.org

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="main-sidebar">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="{{ route('admin.dashboard') }}">FlixyGO</a>
+            <a href="{{ route('admin.dashboard') }}">{{ config('app.name') }}</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="{{ route('admin.dashboard') }}">F</a>
+            <a href="{{ route('admin.dashboard') }}">{{ strtoupper(substr(config('app.name'), 0, 1)) }}</a>
         </div>
         <ul class="sidebar-menu">
             @foreach (config('navigation.sidebar', []) as $attribute)
